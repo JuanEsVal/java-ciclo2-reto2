d3a8f7c96b7c78049137740206454db83c915c77
JuanEsVal <juanes.valenciag@gmail.com>
2021-08-01 14:42:23 -0500

    solucion reto 2 ciclo 2

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        /*Carro carro[] = new Carro[5];
+        /*
+        -----------------------------------------------------
+        Prueba 1:
+        -----------------------------------------------------
+        Carro carro[] = new Carro[5];
         
         carro[0] = new Volqueta(100.0, 10.0);
         carro[1] = new Volqueta(200);
@@ -12,8 +16,22 @@ public class App {
         carro[4] = new Volqueta();
 
         PrecioTotal solucion = new PrecioTotal(carro);
-        solucion.mostrarTotales();*/
-
+        solucion.mostrarTotales();
+        -----------------------------------------------------
+        -----------------------------------------------------
+        Resultado a Prueba 1:
+        -----------------------------------------------------
+        Total Carro 19010.0
+        Total Volqueta 10920.0
+        Total Carro Nascar 8090.0
+        -----------------------------------------------------
+        */
+               
+        
+        /*
+        -----------------------------------------------------
+        Prueba 2:
+        -----------------------------------------------------
         Carro carro[] = new Carro[5];
 
         carro[0] = new Volqueta(1000.0, 100.0);
@@ -23,8 +41,16 @@ public class App {
         carro[4] = new Volqueta();
 
         PrecioTotal solucion = new PrecioTotal(carro);
-
         solucion.mostrarTotales();
+        -----------------------------------------------------
+        -----------------------------------------------------
+        Resultado a Prueba 2:
+        -----------------------------------------------------
+        Total Carro 2054010.0
+        Total Volqueta 804920.0
+        Total Carro Nascar 1249090.0
+        -----------------------------------------------------
+        */
 
 
     }
